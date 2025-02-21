ca5ab5f145997a3a92865ff15f2e23d2d208b955
Mohammed Hassan 🍏 <mohammed.hassan.aboelwafa@sap.com>
2025-02-21 19:02:22 +0100

    update

--- Package.swift
+++ Package.swift
@@ -33,11 +33,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.1"),
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", exact: "600.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", exact: "600.0.0"), //
         .package(url: "https://github.com/Mohaboelwafa1/SourceKitten-iOS13.git", branch: "main"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.6"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0"),
-        .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", from: "0.2.0"),
+        .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", from: "0.2.0"), //
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.8.4")),
     ],
     targets: [
