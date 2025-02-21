4128fc5503650502947dca252e24a6bd5ac54324
Mohammed Hassan 🍏 <mohammed.hassan.aboelwafa@sap.com>
2025-02-21 18:41:56 +0100

    refer to SourceKitten-iOS13 fork

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.1"),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", exact: "600.0.0"),
-        .package(url: "https://github.com/jpsim/SourceKitten.git", .upToNextMinor(from: "0.35.0")),
+        .package(url: "https://github.com/Mohaboelwafa1/SourceKitten-iOS13.git", branch: "main"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.6"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0"),
         .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", from: "0.2.0"),
@@ -85,7 +85,7 @@ let package = Package(
             dependencies: [
                 .product(name: "CryptoSwift", package: "CryptoSwift", condition: .when(platforms: [.linux])),
                 .target(name: "DyldWarningWorkaround", condition: .when(platforms: [.macOS])),
-                .product(name: "SourceKittenFramework", package: "SourceKitten"),
+                .product(name: "SourceKittenFramework", package: "SourceKitten-iOS13"),
                 .product(name: "SwiftIDEUtils", package: "swift-syntax"),
                 .product(name: "SwiftOperators", package: "swift-syntax"),
                 .product(name: "SwiftParser", package: "swift-syntax"),
